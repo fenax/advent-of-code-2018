03d7b30525d141534ac37916bd7e36e1941e90a3
Kasil <kasil@loutre.info>
2018-12-02 00:10:40 +0100

    second star, and some change in parsing, now in main

--- src/day1_chronal_calibration.rs
+++ src/day1_chronal_calibration.rs
@@ -1,16 +1,41 @@
+use std::vec::*;
 
-pub fn process(input: &String) -> i32{
-   input.split('\n').map(|x| x.trim())
-                   .map(|x| -> i32 {x.parse().unwrap_or(0)}).sum() 
+
+pub fn process(input: &Vec<i32>) -> i32{
+   input.iter().sum() 
 }
+
+pub fn process2(input: &Vec<i32>) -> i32{
+   let mut tot = 0;
+   let mut store: Vec<i32> = Vec::new();
+   for x in input.iter().cycle()
+   {
+       store.push(tot);
+       tot+=x; 
+       //println!(" {}",tot);
+       match store.iter().find(|&&x| tot == x) {
+           Some(x) => {eprintln!(" took {} steps", store.len()); return *x},
+           None    => continue,
+       }
+   };
+   panic!("did not find anything {:?}",store);
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test] 
     fn day_one_works() {
-        assert_eq!(process("+1, +1, +1"), 3);
-        assert_eq!(process("+1, +1, -2"), 0);
-        assert_eq!(process("-1, -2, -3"),-6);
+        assert_eq!(process(&vec![ 1, 1, 1]), 3);
+        assert_eq!(process(&vec![ 1,  1, -2]), 0);
+        assert_eq!(process(&vec![-1, -2, -3]),-6);
+    }
+    #[test]
+    fn day_one_1_works() {
+        assert_eq!(process2(&vec![1, -1]),0);
+        assert_eq!(process2(&vec![3, 3, 4, -2, -4]),10);
+        assert_eq!(process2(&vec![-6, 3,8, 5, -6]),5);
+        assert_eq!(process2(&vec![7, 7, -2, -7, -4]),14);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use std::fs::read_to_string;
 use advent_of_code_2018::*;
 fn main() {
     println!("Let's save christmas");
-    
-println!("{}",day1_chronal_calibration::process(&read_to_string("day1-1.input").unwrap()));
+     
+    let input_day1 = read_to_string("day1-1.input").unwrap().split('\n').map(|x| 
+x.trim())
+                                                   .filter_map(|s| 
+s.parse().ok()).collect();
+println!("{}",day1_chronal_calibration::process(&input_day1));
+println!("{}",day1_chronal_calibration::process2(&input_day1));
+
+
+
 }
