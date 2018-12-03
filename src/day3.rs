2d8aa875ba76ba43c36ea4008372fc88e12c8c82
kasil <kasil@loutre.info>
2018-12-04 00:57:13 +0100

    just beginning

---
+++ src/day3.rs
@@ -0,0 +1,12 @@
+fn build_grid(input: &Vec<Vec<i32>>) 
+-> [[u8;1000];1000]{
+    
+    for(i in input){
+        for x in (i[1]..i[1]+i[3]){
+            for y in (i[2]..i[2]+i[4]){
+
+            }
+        }
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 pub mod day1_chronal_calibration;
 pub mod day2_inventory_management_system;
-
+pub mod day3;
